7ae49e47ab041ca9d0676f2c26f76078bed947e7
alaa2amz <alaa2amz@gmail.com>
2026-06-27 13:55:51 +0300

    You will live a long, healthy, happy life and make bags of money.

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 repo=$HOME/workpc
-dot_files="al  .bashrc  .spectrwm.conf .vimrc .xinitrc .config/sxhkd/ ale-map.vim .tmux.con"
+dot_files="al  .bashrc  .spectrwm.conf .vimrc .xinitrc .config/sxhkd/ ale-map.vim .tmux.conf"
 aptpath="/var/log/apt"
 
 for file in $dot_files
